338652852a19b238a06f553cd3afce28a0f72f76
Sebasflox <sebas33115@gmail.com>
2021-06-27 16:39:57 -0600

    Update ProyectoFinal.java

--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -83,11 +83,6 @@ public class ProyectoFinal {
         
         System.out.println("Usted nació el: " + usuario.getdia() + " de " + usuario.getmes() + " del año " + usuario.getaño());
         
-        System.out.println("Su usuario es: " + usuario.getuser() + " y su password es: " + usuario.getcontraseña());  
-        
-        System.out.println("Hola guapo");
-        
-        System.out.println("hola");
+        System.out.println("Su usuario es: " + usuario.getuser() + " y su password es: " + usuario.getcontraseña());    
     }
-    
 }
