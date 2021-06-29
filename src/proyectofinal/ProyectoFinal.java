314b1b17735a5035ad644dce004740818f22d19d
Sebasflox <sebas33115@gmail.com>
2021-06-28 18:12:25 -0600

    Update ProyectoFinal.java

--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package proyectofinal;
 
 import java.util.Scanner;
 
-/**
- *
- * @author 
- */
+
 public class ProyectoFinal {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         String nombre;
         String apellido1;
