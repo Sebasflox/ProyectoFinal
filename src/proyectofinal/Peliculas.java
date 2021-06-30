2bfc8a7624de3bca808a5d98acd4c0962663d8af
soulrosh <herreragomezerlingrolando@gmail.com>
2021-06-29 18:11:08 -0600

    Update Peliculas.java

--- src/proyectofinal/Peliculas.java
+++ src/proyectofinal/Peliculas.java
@@ -9,4 +9,46 @@ public class Peliculas {
     private String actor;
     private String actriz;
     private String genero;
+    
+    public String getnombrepelicula(){
+    return nombrepelicula;
+    }
+    public void setnombrepelicula(String Nombrepelicula ){
+    nombrepelicula=Nombrepelicula;
+    }
+    
+    public String gettitulo(){
+    return titulo;    
+    }
+    public void settitulo(String Titulo){
+    titulo=Titulo;
+    }
+    
+    public String getdirector(){
+    return director;    
+    }
+    public void setdirector(String Director){
+    director=Director;
+    }
+    
+    public String getactor(){
+    return actor;
+    }
+    public void setactor(String Actor){
+    actor=Actor;
+    }
+    
+    public String getactriz(){
+    return actriz;
+    }
+    public void setactriz(String Actriz){
+    actriz=Actriz;
+    }
+    
+    public String getgenero(){
+    return genero;
+    }
+    public void setgenero(String Genero){
+    genero=Genero;
+    }
 }
