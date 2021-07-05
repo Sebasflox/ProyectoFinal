8dae414952807f6e1982d07cb7373811b1a1c04b
Sebasflox <sebas33115@gmail.com>
2021-07-05 14:52:41 -0600

    Create sedes.java

---
+++ src/Clases/sedes.java
@@ -0,0 +1,26 @@
+
+package Clases;
+
+
+public class sedes {
+    
+    private String nombre_sede;
+    private String direccion;
+    
+    
+    public String getnombre_sede(){
+    return nombre_sede;
+    }
+    public void setnombre_sede(String Nombre_sede){
+    nombre_sede=Nombre_sede;
+    }
+    
+    public String getdireccion(){
+    return direccion;
+    }
+    public void setdireccion(String Direccion){
+    direccion=Direccion;
+    }
+    
+    
+}
