14b1dde66d42b6676658ca1096441a93bb338dbc
Sebasflox <sebas33115@gmail.com>
2021-08-10 22:45:27 -0600

    Create LecturaArchivo.java

---
+++ src/Clases/LecturaArchivo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import proyectofinal.MostrarUsuarios;
+
+/**
+ *
+ * @author Sebas
+ */
+public class LecturaArchivo {
+    
+    
+    public static void main(String[] args){
+        
+        MostrarUsuarios ventana2 = new MostrarUsuarios();
+        ventana2.setVisible(true);
+    
+    }
+    
+    
+}
